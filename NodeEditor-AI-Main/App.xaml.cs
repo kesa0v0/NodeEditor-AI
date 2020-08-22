2e72d72e7c181f626ab140f8c25164f9b2e00c99
kesa <kesa0v0@gmail.com>
2020-08-23 01:14:20 +0900

    init

---
+++ NodeEditor-AI-Main/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NodeEditor_AI_Main
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ NodeEditor-AI-Main/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DynamicData;
+using NodeNetwork.ViewModels;
+
+namespace NodeEditor_AI_Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var network = new NetworkViewModel();
+
+            //Create the node for the first node, set its name and add it to the network.
+            var node1 = new NodeViewModel {Name = "Node 1"};
+            network.Nodes.Add(node1);
+
+            //Create the viewmodel for the input on the first node, set its name and add it to the node.
+            var node1Input = new NodeInputViewModel {Name = "Node 1 input"};
+            node1.Inputs.Add(node1Input);
+
+            //Create the second node viewmodel, set its name, add it to the network and add an output in a similar fashion.
+            var node2 = new NodeViewModel {Name = "Node 2"};
+            network.Nodes.Add(node2);
+
+            var node2Output = new NodeOutputViewModel {Name = "Node 2 output"};
+            node2.Outputs.Add(node2Output);
+
+            //Assign the viewmodel to the view.
+            networkView.ViewModel = network;
+        }
+    }
+}
