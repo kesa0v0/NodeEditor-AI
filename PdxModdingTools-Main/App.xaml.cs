9d250fa9bd373467e3f2d9caf9355fd1d67ffb7e
kesa <kesa0v0@gmail.com>
2020-09-08 19:48:09 +0900

    add main window (will be menu)

---
+++ PdxModdingTools-Main/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PdxModdingTools_Main
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ PdxModdingTools-Main/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace PdxModdingTools_Main
+{
+    public partial class MainWindow
+    {
+        NodeEditor_PdxEventChain_Main.MainWindow _stellarisEventManagerForm;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            _stellarisEventManagerForm= new NodeEditor_PdxEventChain_Main.MainWindow();
+            _stellarisEventManagerForm.Show();
+            Close();
+        }
+    }
+}
