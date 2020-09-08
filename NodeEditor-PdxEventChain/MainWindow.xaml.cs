3a7e9d1406475a2023fd7029af0367b380877b8a
kesa <kesa0v0@gmail.com>
2020-09-08 21:45:29 +0900

    add folding manager
    but it's xml version so i should make brace version.

--- NodeEditor-PdxEventChain/MainWindow.xaml.cs
+++ NodeEditor-PdxEventChain/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using NodeNetwork.ViewModels;
+﻿using System;
+using System.Windows.Threading;
+using ICSharpCode.AvalonEdit.Folding;
+using NodeNetwork.ViewModels;
 
 namespace NodeEditor_PdxEventChain_Main
 {
@@ -7,9 +10,31 @@ namespace NodeEditor_PdxEventChain_Main
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            Console.Write(@"test!");
+
             var network = new NetworkViewModel();
             NetworkView.ViewModel = network;
+
+            DispatcherTimer foldingUpdateTimer = new DispatcherTimer();
+            foldingUpdateTimer.Interval = TimeSpan.FromSeconds(foldingDuration);
+            foldingUpdateTimer.Tick += delegate { UpdateFoldings(); };
+            foldingUpdateTimer.Start();
+
+            foldingManager = FoldingManager.Install(CodeEditor.TextArea);
+            foldingStrategy = new XmlFoldingStrategy();
         }
+
+        #region MyRegion
+
+        private FoldingManager foldingManager;
+        private XmlFoldingStrategy foldingStrategy;
+        private double foldingDuration = 0.5;
+        
+        void UpdateFoldings()
+        {
+            foldingStrategy.UpdateFoldings(foldingManager, CodeEditor.Document);
+        }
+        #endregion
     }
 }
